07652c7d16c8796d8bf88376aad900b50a981248
sima ziyaei <sziyaei2000@gmail.com>
2022-07-01 15:40:50 +0430

    add post method

--- main.js
+++ main.js
@@ -5,8 +5,29 @@ $(document).ready(function () {
   $.ajax({ url: URL_API }).done(function (toDos) {
     $.each(toDos, function (index, Todo) {
        div.append (
-        `<div>${Todo.toDo}</div>`
+        `<div data-index=${index}>${Todo.toDo}</div>`
        )  
     });
   });
 });
+
+$("#button-addon2").click(function(){
+
+let obj = { toDo: `${$(".inp").val()}` }
+ addToDo(obj)});
+
+function addToDo(obj) {
+  $.ajax({
+    url: URL_API,
+    type: 'POST',
+    data: JSON.stringify(obj),
+    contentType: 'application/json; charset=utf-8',
+    success: function (data) {
+      div.append (
+        `<div>${data.toDo}</div>`
+       )
+    },
+  });
+}
+;
+
