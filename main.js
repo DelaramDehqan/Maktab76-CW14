47db916f8fee42f9e0fec346e83f40da29178dd6
SamanehBahrami <bahrami.s0288@gmail.com>
2022-07-01 16:41:01 +0430

    added cross & tick

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 const URL_API = "https://62b40bbda36f3a973d2ab0ed.mockapi.io/toDoList";
-const div = $("#div");
-
+const content = $("#content");
+function todoDom(Todo){
+  content.append(
+    `<div class ="content" data-id=${Todo.id}>
+    <div style = ${Todo.status} ? "display:block;" : "display:none;">
+    <i class="bi bi-check"></i></div>
+    <div >${Todo.toDo}</div>
+    <div><i class="bi bi-x"></i></div></div>
+    `
+  );
+}
 $(document).ready(function () {
   $.ajax({ url: URL_API }).done(function (toDos) {
-    $.each(toDos, function (index, Todo) {
-       div.append (
-        `<div data-index=${index}>${Todo.toDo}</div>`
-       )  
+    $.each(toDos, function (_, Todo) {
+      todoDom(Todo)
     });
   });
 });
 
-$("#button-addon2").click(function(){
-
-let obj = { toDo: `${$(".inp").val()}` }
- addToDo(obj)});
+$("#button-addon2").click(function () {
+  let obj = { toDo: `${$(".inp").val()}` };
+  addToDo(obj);
+});
 
 function addToDo(obj) {
   $.ajax({
     url: URL_API,
-    type: 'POST',
+    type: "POST",
     data: JSON.stringify(obj),
-    contentType: 'application/json; charset=utf-8',
+    contentType: "application/json; charset=utf-8",
     success: function (data) {
-      div.append (
-        `<div>${data.toDo}</div>`
-       )
+      todoDom(data)
     },
   });
 }
-;
-
